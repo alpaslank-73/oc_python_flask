e3dc1cefa2cc2a789410a4cbbf4ef9bdc8beaeb7
Alpaslan Kaplan <alp@perception.com.tr>
2020-11-06 18:42:11 +0300

    added health check

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request, session, redirect, url_for, g, Response
+
+#from flask import Flask, request
+
 
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
 def home():
     return ('Hello World')
+
+@app.route('/healthz', methods = ['GET'])
+def healthz():
+    resp = app.make_response('ok')
+    resp.headers['Custom-Header'] = 'Awesome'
+    return resp
 
 if __name__ == '__main__':
     app.run()
